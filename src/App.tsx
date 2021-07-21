d31d787c631d804ee11c1dede34717e6e174cfdc
lelouchB <ashutoshs.ic18@nsut.ac.in>
2021-07-21 17:46:25 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import GameCard, { DataProps } from "./components/GameCard";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<DataProps[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [companies, setCompanies] = useState<string[]>([]);
+  const [selectedCompany, setSelectedCompany] = useState<string>("");
+
+  const getData = () => {
+    setLoading(true);
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setData(json);
+        setCompanies(
+          json.map((item: DataProps) => {
+            return item.company.name;
+          })
+        );
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const search = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  function customSearch(query: string) {
+    return function (elem: DataProps) {
+      var userNames = elem.users.map((user: { name: string }) => {
+        return user.name.toLowerCase();
+      });
+      const subUserNames = userNames.map((name: string) => {
+        return name.includes(query);
+      });
+
+      return elem.id.includes(query) || subUserNames.includes(true);
+    };
+  }
+  let result = data;
+  if (searchTerm) {
+    result = data.filter(customSearch(searchTerm.toLowerCase()));
+  }
+
+  if (selectedCompany) {
+    result = data.filter((item: DataProps) => {
+      return item.company.name === selectedCompany;
+    });
+  }
+  const clear = () => {
+    setSelectedCompany("");
+    setSearchTerm("");
+  };
+
+  if (loading) {
+    return <div> Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React + TS Game Room</h1>
+      <div className="App-menu">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Enter item to be searched"
+            onChange={(e) => search(e)}
+            value={searchTerm}
+          />
+        </div>
+        <div className="App-filter">
+          <div className="App-filter-date">
+            <button onClick={clear} className="clear-button">
+              Clear
+            </button>
+          </div>
+        </div>
+        <div className="App-filter-company">
+          <select
+            defaultValue="default"
+            onChange={(e) => setSelectedCompany(e.target.value)}
+          >
+            <option value="default" disabled>
+              Filter by Company Name
+            </option>
+
+            {companies &&
+              companies.length > 0 &&
+              companies.map((company: any, index: number) => (
+                <option key={index} value={company}>
+                  {company}
+                </option>
+              ))}
+          </select>
+        </div>
+      </div>
+      {data &&
+        result.length > 0 &&
+        result.map((item) => (
+          <GameCard
+            key={item.id}
+            id={item.id}
+            scheduledTime={item.scheduledTime}
+            users={item.users}
+            company={item.company}
+            status={item.status}
+          />
+        ))}
     </div>
   );
 }
---
+++ src/components/GameCard.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./GameCard.css";
+
+export type DataProps = {
+  id: string;
+  scheduledTime: string;
+  company: { id: string; name: string };
+  users: {
+    id: string;
+    name: string;
+    email: string;
+  }[];
+  status: string;
+};
+
+// type StatusColors = {
+//   scheduled: string;
+//   in_progress: string;
+//   done: string;
+//   issue: string;
+// };
+
+const GameCard: React.FC<DataProps> = ({
+  id,
+  scheduledTime,
+  company,
+  users,
+  status,
+}) => {
+
+    const scdTime:Date = new Date(scheduledTime)
+
+    return (
+    <div className="card">
+      <div className="card--grid">
+        <div className="card--grid-status">
+          <div className={`card--grid-status-${status}`}> </div>
+        </div>
+        <div className="card--grid-info">
+          <span>{`#${id}`}</span>
+          <span> {scdTime.toDateString()}, {scdTime.getHours()}:{scdTime.getMinutes()}</span>
+          <span> {company.name}</span>
+        </div>
+        <div className="card--content">
+          <div className="card--username">
+            {users.map((user) => user.name).join(", ")}
+          </div>
+          <div className="card--status">{status}</div>{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default GameCard;
